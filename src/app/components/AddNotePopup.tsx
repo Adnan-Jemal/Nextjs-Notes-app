1172c7f14834d54835d442dde7ed8a8381059a9c
adn-abdu <134360306+Adnan-jemal@users.noreply.github.com>
2023-11-16 13:16:20 +0300

    fixed add note popup

--- src/app/components/AddNotePopup.tsx
+++ src/app/components/AddNotePopup.tsx
@@ -1,23 +1,28 @@
-'use client'
-import React from 'react'
-import AddBtn from './AddBtn'
-import { useRouter } from 'next/navigation'
+"use client";
+import React from "react";
+import AddBtn from "./AddBtn";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
-type props={
-  Opened:Boolean
-}
-const AddNotePopup = ({Opened}:props) => {
+type props = {
+  Opened: Boolean;
+};
+const AddNotePopup = ({ Opened }: props) => {
   const [opened, setOpened] = useState(false);
-  const route = useRouter()
+  const route = useRouter();
   const HandelAdd = () => {
-    setOpened(!opened)
-    console.log(opened)
-    
-    
+    setOpened(!opened);
+    console.log(opened);
   };
-  return (<><AddBtn Onclick={HandelAdd} Opened={opened}/> {opened&&<div className=' z-20 min-h-full   absolute bottom-0 top-0 left-0 right-0 backdrop-blur-lg  overflow-hidden'>AddNotePopup</div>}</>
-    
-  )
-}
+  return (
+    <>
+      <AddBtn Onclick={HandelAdd} Opened={opened} />{" "}
+      {opened && (
+        <div className=" z-20 min-h-screen   fixed bottom-0 top-0 left-0 right-0 backdrop-blur-lg  overflow-hidden">
+          AddNotePopup
+        </div>
+      )}
+    </>
+  );
+};
 
-export default AddNotePopup
+export default AddNotePopup;
